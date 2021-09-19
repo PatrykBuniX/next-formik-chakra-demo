fbab7c130dc82d45b62725fecd537e9537f8ff88
PatrykBuniX <patrykbunix@gmail.com>
2021-09-19 16:43:09 +0200

    Add simple email validation.

--- components/forms/SignUpForm.tsx
+++ components/forms/SignUpForm.tsx
@@ -17,6 +17,8 @@ export const SignUpForm = () => {
     }
     if (!values.email) {
       errors.email = "Email is required";
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      errors.email = "Invalid email address";
     }
     if (!values.password) {
       errors.password = "Password is required";
