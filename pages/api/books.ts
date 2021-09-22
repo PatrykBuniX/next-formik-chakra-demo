c572ac8813170f16af20cb8abc1ca8d1f5510316
PatrykBuniX <patrykbunix@gmail.com>
2021-09-22 17:49:58 +0200

    Load books from postgres.

--- pages/api/books.ts
+++ pages/api/books.ts
@@ -1,40 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import jwt from "jsonwebtoken";
-
-const books = [
-  {
-    author: "Chinua Achebe",
-    country: "Nigeria",
-    language: "English",
-    pages: 209,
-    title: "Things Fall Apart",
-    year: 1958,
-  },
-  {
-    author: "Hans Christian Andersen",
-    country: "Denmark",
-    language: "Danish",
-    pages: 784,
-    title: "Fairy tales",
-    year: 1836,
-  },
-  {
-    author: "Dante Alighieri",
-    country: "Italy",
-    language: "Italian",
-    pages: 928,
-    title: "The Divine Comedy",
-    year: 1315,
-  },
-];
+import { prisma } from "../../prisma/client";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const token = req.headers.authorization?.split(" ")[1];
   if (token) {
-    jwt.verify(token, process.env.NEXT_PUBLIC_ACCESS_TOKEN_SECRET!, (err) => {
+    jwt.verify(token, process.env.NEXT_PUBLIC_ACCESS_TOKEN_SECRET!, async (err) => {
       if (err) {
         return res.status(403).json({ message: err.message });
       }
+      const books = await prisma.books.findMany();
       res.status(200).json({ books });
     });
   } else {
