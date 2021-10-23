c9c3717a1a16da69410570d7741764c7448afcb2
PatrykBuniX <patrykbunix@gmail.com>
2021-10-23 10:43:19 +0200

    Display books for logged in users.

--- components/books-list/BooksList.tsx
+++ components/books-list/BooksList.tsx
@@ -1,5 +1,5 @@
 import type { Book } from "../../types";
-import { UnorderedList, ListItem, Heading, Text, HStack } from "@chakra-ui/react";
+import { UnorderedList, ListItem, Heading, Text, HStack, VStack } from "@chakra-ui/react";
 
 type Props = {
   books: Book[];
@@ -8,21 +8,24 @@ type Props = {
 export const BooksList = ({ books }: Props) => {
   if (books.length < 1) return <Text>There are no books in the shelve</Text>;
   return (
-    <UnorderedList listStyleType="none" spacing={5}>
-      {books.map((book) => (
-        <ListItem boxShadow="md" p={5} background="gray.100" borderRadius="md" key={book.title}>
-          <Heading fontWeight="bold" size="md">
-            {book.title}
-          </Heading>
-          <Text fontSize="medium" color="gray.500">
-            by {book.author} ({book.year})
-          </Text>
-          <HStack spacing={1} flexWrap="wrap" justifyContent="space-between" mt={3}>
-            <Text>{book.language}</Text>
-            <Text>{book.pages} pages</Text>
-          </HStack>
-        </ListItem>
-      ))}
-    </UnorderedList>
+    <VStack spacing={10}>
+      <Heading>Books:</Heading>
+      <UnorderedList mt={200} listStyleType="none" spacing={5}>
+        {books.map((book) => (
+          <ListItem boxShadow="md" p={5} background="gray.100" borderRadius="md" key={book.title}>
+            <Heading fontWeight="bold" size="md">
+              {book.title}
+            </Heading>
+            <Text fontSize="medium" color="gray.500">
+              by {book.author} ({book.year})
+            </Text>
+            <HStack spacing={1} flexWrap="wrap" justifyContent="space-between" mt={3}>
+              <Text>{book.language}</Text>
+              <Text textDecoration="underline">{book.pages} pages</Text>
+            </HStack>
+          </ListItem>
+        ))}
+      </UnorderedList>
+    </VStack>
   );
 };
--- components/dashboard/LoggedInDashboard.tsx
+++ components/dashboard/LoggedInDashboard.tsx
@@ -1,13 +1,16 @@
 import { useState, useEffect } from "react";
 import { VStack, Text, Button } from "@chakra-ui/react";
 import { getMe } from "../../apiHelpers/getMe";
-import { User } from "../../types";
+import { getBooks } from "../../apiHelpers/getBooks";
+import { User, Book } from "../../types";
 import { useAuth } from "../../context/AuthContext";
 import { deleteAccount } from "../../apiHelpers/deleteAccount";
+import { BooksList } from "../books-list/BooksList";
 
 export const LoggedInDashboard = () => {
   const { accessToken, logOutUser } = useAuth();
   const [userData, setUserData] = useState<User | null>(null);
+  const [books, setBooks] = useState<Book[]>([]);
 
   useEffect(() => {
     if (!accessToken) {
@@ -29,6 +32,16 @@ export const LoggedInDashboard = () => {
     }
   };
 
+  const handleBooksLoad = async () => {
+    if (!accessToken) {
+      setBooks([]);
+      return;
+    }
+    getBooks(accessToken)
+      .then(({ books }) => setBooks(books))
+      .catch(console.log);
+  };
+
   if (!userData) return <p>No user info</p>;
 
   return (
@@ -39,9 +52,13 @@ export const LoggedInDashboard = () => {
       <Text>
         {userData.firstName} {userData.lastName}
       </Text>
-      <Button onClick={logOutUser}>logout</Button>
-      <Button colorScheme="red" focusBorderColor="lime" onClick={handleAccountDelete}>
+      <Button onClick={logOutUser}>Logout</Button>
+      <Button colorScheme="red" onClick={handleAccountDelete}>
         Delete account
+      </Button>
+      <BooksList books={books} />
+      <Button colorScheme="blue" onClick={handleBooksLoad}>
+        Load books
       </Button>
     </VStack>
   );
